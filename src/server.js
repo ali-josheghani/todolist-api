ef8bb29ea09f98d36fbb2153c4ca62b2423ef3f0
ALi Josheghani <ali.josheghani2005@gmail.com>
2024-10-03 09:41:15 +0330

    Refactor server.ts to dynamically assign port number from environment variable or default to 3000

--- src/server.js
+++ src/server.js
@@ -6,15 +6,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // Import the 'express' module along with 'Request' and 'Response' types from express
 const express_1 = __importDefault(require("express"));
 const get_todolist_1 = __importDefault(require("./routes/handler/get-todolist"));
+const add_todolist_1 = __importDefault(require("./routes/handler/add-todolist"));
+const delete_todolist_1 = __importDefault(require("./routes/handler/delete-todolist"));
 // Create an Express application
 const app = (0, express_1.default)();
 // Specify the port number for the server
-const port = 3000;
+const port = Number(process.env.PORT) || 3000;
+app.use(express_1.default.json());
 // Define a route for the root path ('/')
-app.get('/', (req, res) => {
-    res.redirect('/todolist');
+app.get("/", (req, res) => {
+    res.redirect("/todolist");
 });
-app.get('/todolist', get_todolist_1.default);
+app.get("/todolist", get_todolist_1.default);
+app.post("/todolist", add_todolist_1.default);
+app.delete("/todolist/:id", delete_todolist_1.default);
 // Start the server and listen on the specified port
 app.listen(port, () => {
     // Log a message when the server is successfully running
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 // Import the 'express' module along with 'Request' and 'Response' types from express
-import express from 'express';
-import get_todolist from './routes/handler/get-todolist';
+import express from "express";
+import get_todolist from "./routes/handler/get-todolist";
+import add_todolist from "./routes/handler/add-todolist";
+import delete_todolist from "./routes/handler/delete-todolist";
 
 // Create an Express application
 const app = express();
@@ -8,12 +10,18 @@ const app = express();
 // Specify the port number for the server
 const port: number = Number(process.env.PORT) || 3000;
 
-// Define a route for the root path ('/')
-app.get('/', (req,res) =>{
-  res.redirect('/todolist');
-} );
+app.use(express.json());
 
-app.get('/todolist', get_todolist);
+// Define a route for the root path ('/')
+app.get("/", (req, res) => {
+  res.redirect("/todolist");
+});
+
+app.get("/todolist", get_todolist);
+
+app.post("/todolist", add_todolist);
+
+app.delete("/todolist/:id", delete_todolist);
 
 // Start the server and listen on the specified port
 app.listen(port, () => {
